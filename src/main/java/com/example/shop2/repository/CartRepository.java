4fdf5ff8b80bb84a076f005766798afc4c2c4ca2
riptae <riptaggart@naver.com>
2024-04-11 00:47:57 +0900

    item 추가

---
+++ src/main/java/com/example/shop2/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package com.example.shop2.repository;
+
+import com.example.shop2.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    Cart findByMemberId(Long memberId);
+
+}
